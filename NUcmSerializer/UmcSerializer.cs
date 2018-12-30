69fff7001b6d59bf8d46ac3b7803f878f2a68036
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2018-12-30 19:41:35 +0100

    ucm: Initial implementation of UmcSerializer class
    Currently only Serialize method is defined with Deserialize planned to
    be added in the future.
    
    Constructor of said serializer should be also able to check correctness
    of "semantics" for provided topology, that is, its structure and
    attributes applied.
    ALSA topology configuration has several rules that must be obeyed in
    order to have it properly converted into binary format later by alsatplg
    or any other valid tool.
    
    Signed-off-by: Cezary Rojewski <cezary.j.rojewski@gmail.com>

---
+++ NUcmSerializer/UmcSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NUmcSerializer
+{
+    public class UmcSerializer
+    {
+        public UmcSerializer()
+        {
+            // TODO: check semantics of type to be serialized/ deserialized
+        }
+
+        public void Serialize(Stream stream, IEnumerable<Section> topology)
+        {
+            if (topology == null)
+                throw new ArgumentNullException("topology");
+
+            var writer = new UmcTextWriter(stream, null);
+
+            foreach (var section in topology)
+                writer.WriteToken(section, null);
+            writer.Flush();
+            writer.Close();
+        }
+
+        public IEnumerable<Section> Deserialize(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
