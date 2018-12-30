e1c51aa93418cf6cbf4a41c53c151e372080cc3c
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2018-12-30 19:34:28 +0100

    ucm: Implement UmcTextWriter class
    Core of UmcTextWriter is based on XmlTextWriter implementation.
    Declares TokenType and TokenInfo types which provide info regarding
    token that's being written.
    
    This patch also implements TypeHelper helper class which provides
    extension methods for Type type, required by the writer.
    
    Serialization supports only class properties, although nothing else
    should be required as we are speaking here about serialization of
    quite simple and dated syntax, UMC.
    
    Signed-off-by: Cezary Rojewski <cezary.j.rojewski@gmail.com>

---
+++ NUcmSerializer/TypeHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NUmcSerializer
+{
+    public static class TypeHelper
+    {
+        public static bool IsSimpleType(this Type type)
+        {
+            return type.IsPrimitive || type.IsValueType || type == typeof(string);
+        }
+
+        public static bool IsSimpleArrayType(this Type type)
+        {
+            return type.IsArray && IsSimpleType(type.GetElementType());
+        }
+
+        public static TokenType GetTypeTokenType(this Type type)
+        {
+            if (type.IsSimpleType())
+                return TokenType.Element;
+            else if (type.IsSimpleArrayType())
+                return TokenType.Array;
+            else if (type.IsSubclassOf(typeof(Section)))
+                return TokenType.Section;
+
+            return TokenType.None;
+        }
+    }
+}
---
+++ NUcmSerializer/UmcTextWriter.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace NUmcSerializer
+{
+    public enum TokenType
+    {
+        None,
+        Element,
+        Array,
+        Section
+    }
+
+    public class UmcTextWriter
+    {
+        TextWriter writer;
+        Encoding encoding;
+        char indentChar;
+        int indentSize;
+
+        TokenInfo[] stack;
+        int top;
+
+        public struct TokenInfo
+        {
+            public string Name;
+            public TokenType Type;
+            public string Identifier;
+
+            public void Init(object token, PropertyInfo tokenInfo)
+            {
+                Type type = token.GetType();
+                object[] attrs;
+
+                Type = type.GetTypeTokenType();
+
+                if (tokenInfo == null)
+                    attrs = type.GetCustomAttributes(false);
+                else
+                    attrs = tokenInfo.GetCustomAttributes(false);
+
+                var attr = (UmcNamedTagAttribute)attrs.SingleOrDefault(
+                    a => a.GetType().IsSubclassOf(typeof(UmcNamedTagAttribute))
+                );
+
+                // obtain token name
+                if (attr != null && attr.Name != null)
+                    Name = attr.Name;
+                else if (tokenInfo != null)
+                    Name = tokenInfo.Name;
+                else
+                    Name = type.Name;
+
+                PropertyInfo propInfo = type.GetProperties().SingleOrDefault(
+                    p => Attribute.IsDefined(p, typeof(UmcIdentifierAttribute))
+                );
+
+                if (propInfo != null)
+                    Identifier = (string)propInfo.GetValue(token);
+            }
+        }
+
+        public UmcTextWriter(Stream stream, Encoding encoding)
+        {
+            this.encoding = encoding;
+            if (encoding != null)
+                writer = new StreamWriter(stream, encoding);
+            else
+                writer = new StreamWriter(stream);
+            stack = new TokenInfo[8];
+            top = 0;
+            indentChar = ' ';
+            indentSize = 4;
+        }
+
+        // helper method, retrieves token properties to enumerate over
+        static PropertyInfo[] GetTypeTokenProperties(object token)
+        {
+            Type type = token.GetType();
+            PropertyInfo[] props = type.GetProperties();
+
+            List<PropertyInfo> result = props.Where(
+                p => !Attribute.IsDefined(p, typeof(UmcIdentifierAttribute))
+            ).ToList();
+
+            return result.ToArray();
+        }
+
+        internal void WriteValue(object token)
+        {
+            writer.Write(token.ToString().ToLower());
+        }
+
+        internal void WriteArray(object token)
+        {
+            Array array = (Array)token;
+            StringBuilder str = new StringBuilder();
+
+            foreach (var elem in array)
+            {
+                str.Clear();
+                str.Append(indentChar, indentSize * top);
+                str.Append("\"");
+                str.Append(elem);
+                str.Append("\"");
+                writer.WriteLine(str);
+            }
+        }
+
+        public void WriteToken(object token, PropertyInfo tokenInfo)
+        {
+            if (token == null)
+                throw new ArgumentNullException("token");
+
+            TokenInfo current = new TokenInfo();
+            current.Init(token, tokenInfo);
+
+            WriteTagStart(current);
+
+            switch (current.Type)
+            {
+                case TokenType.Element:
+                    WriteValue(token);
+                    break;
+
+                case TokenType.Array:
+                    WriteArray(token);
+                    break;
+
+                case TokenType.Section:
+                    PropertyInfo[] infos = GetTypeTokenProperties(token);
+
+                    foreach (var info in infos)
+                        WriteToken(info.GetValue(token), info);
+                    break;
+
+                default:
+                    throw new NotSupportedException("current");
+            }
+
+            WriteTagEnd();
+        }
+
+        public void WriteTagStart(TokenInfo tokenInfo)
+        {
+            if (tokenInfo.Type == TokenType.None)
+                throw new InvalidOperationException("tokenInfo");
+
+            StringBuilder str = new StringBuilder();
+            str.Append(indentChar, indentSize * top);
+            str.Append(tokenInfo.Name);
+            if (tokenInfo.Identifier != null)
+            {
+                str.Append(".\"");
+                str.Append(tokenInfo.Identifier);
+                str.Append("\"");
+            }
+
+            switch (tokenInfo.Type)
+            {
+                case TokenType.Element:
+                    str.Append(" \"");
+                    writer.Write(str);
+                    break;
+
+                case TokenType.Array:
+                    writer.WriteLine();
+                    str.Append(" [");
+                    writer.WriteLine(str);
+                    break;
+
+                case TokenType.Section:
+                    writer.WriteLine();
+                    str.Append(" {");
+                    writer.WriteLine(str);
+                    break;
+
+                default:
+                    throw new NotSupportedException("tokenInfo");
+            }
+
+            PushStack(tokenInfo);
+        }
+
+        public void WriteTagEnd()
+        {
+            if (top <= 0)
+                throw new InvalidOperationException("top");
+            TokenInfo current = stack[top];
+            top--; // pop stack
+
+            switch (current.Type)
+            {
+                case TokenType.Element:
+                    writer.Write("\"");
+                    break;
+
+                case TokenType.Array:
+                case TokenType.Section:
+                    StringBuilder str = new StringBuilder();
+                    str.Append(indentChar, indentSize * top);
+                    writer.Write(str);
+
+                    if (current.Type != TokenType.Section)
+                        writer.Write("]");
+                    else
+                        writer.Write("}");
+                    break;
+
+                default:
+                    throw new NotSupportedException("current");
+            }
+
+            writer.WriteLine();
+        }
+
+        void PushStack(TokenInfo tokenInfo)
+        {
+            if (top == stack.Length - 1)
+            {
+                TokenInfo[] na = new TokenInfo[stack.Length * 2];
+                if (top > 0) Array.Copy(stack, na, top + 1);
+                stack = na;
+            }
+
+            top++; // Move up stack
+            stack[top] = tokenInfo;
+        }
+
+        // Flushes whatever is in the buffer to the underlying stream/TextWriter and flushes the underlying stream/TextWriter.
+        public void Flush()
+        {
+            writer.Flush();
+        }
+
+        public void AutoCompleteAll()
+        {
+            while (top > 0)
+                WriteTagEnd();
+        }
+
+        public void Close()
+        {
+            try
+            {
+                AutoCompleteAll();
+            }
+            catch
+            {
+            }
+
+            writer.Close();
+        }
+    }
+}
