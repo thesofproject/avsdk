c5915adc387db3602036fdc966a41079ff6539e0
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2018-12-30 19:12:54 +0100

    ucm: Define base Umc serialization attributes
    Objects found within topology configuration file could be divided into
    three groups: elements, arrays and sections.
    
    This change defines attributes for all of these, plus an unique
    UmcIdentifier attribute which goal, once applied, is to make given
    Section's property responsible for setting its identifier field.
    
    Signed-off-by: Cezary Rojewski <cezary.j.rojewski@gmail.com>

---
+++ NUcmSerializer/Attributes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace NUmcSerializer
+{
+    public abstract class UmcNamedTagAttribute : Attribute
+    {
+        public string Name { get; }
+
+        public UmcNamedTagAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+
+    // exclusive with all other attributes, default for all properties
+    public class UmcElementAttribute : UmcNamedTagAttribute
+    {
+        public UmcElementAttribute(string name)
+            : base(name)
+        {
+        }
+
+        public UmcElementAttribute()
+            : this(null)
+        {
+        }
+    }
+
+    // default for all arrays
+    public class UmcArrayAttribute : UmcNamedTagAttribute
+    {
+        public UmcArrayAttribute(string name)
+            : base(name)
+        {
+        }
+
+        public UmcArrayAttribute()
+            : this(null)
+        {
+        }
+    }
+
+    // default for all sections
+    public class UmcSectionAttribute : UmcNamedTagAttribute
+    {
+        public UmcSectionAttribute(string name)
+            : base(name)
+        {
+        }
+
+        public UmcSectionAttribute()
+            : this(null)
+        {
+        }
+    }
+
+    // only one per Type (section), exclusive with all other attributes
+    public class UmcIdentifierAttribute : Attribute
+    {
+    }
+}
