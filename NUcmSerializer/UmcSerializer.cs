f1a282796ffacf1716f84e18502b5e4d793e142e
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2019-01-01 15:03:28 +0100

    ucm: Apply license header to all project files
    Propagate copyright and license header wherever applicable.
    
    Signed-off-by: Cezary Rojewski <cezary.j.rojewski@gmail.com>

--- NUcmSerializer/UmcSerializer.cs
+++ NUcmSerializer/UmcSerializer.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿//
+// Copyright (c) 2018, Intel Corporation. All rights reserved.
+//
+// Author: Cezary Rojewski <cezary.rojewski@intel.com>
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms and conditions of the MIT License.
+//
+// This program is distributed in the hope it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.
+//
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 
