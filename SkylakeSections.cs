cfe816349b42f8ad028a4fca707c465eb2977353
Cezary Rojewski <cezary.rojewski@intel.com>
2019-01-15 21:43:08 +0100

    Define Skylake-specific tokens and tuples sections
    Goal of said classes is to provide unified way for instantiating Intel
    specific SectionVendorTokens and SectionVendorTuples classes.
    
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

---
+++ SkylakeSections.cs
@@ -0,0 +1,32 @@
+﻿using NUmcSerializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace itt
+{
+    [UmcSection("SectionVendorTokens")]
+    public class SectionSkylakeTokens : SectionVendorTokens
+    {
+        public const string IDENTIFIER = "skl_tokens";
+
+        public SectionSkylakeTokens()
+        {
+            Identifier = IDENTIFIER;
+
+            IEnumerable<SKL_TKN> values = Enum.GetValues(typeof(SKL_TKN)).Cast<SKL_TKN>();
+            Tokens = values.Select(
+                t => Tuple.Create(t.GetName(), (uint)t)).ToArray();
+        }
+    }
+
+    [UmcSection("SectionVendorTuples")]
+    public class SectionSkylakeTuples : SectionVendorTuples
+    {
+        public SectionSkylakeTuples(string identifier)
+        {
+            Identifier = identifier;
+            Tokens = SectionSkylakeTokens.IDENTIFIER;
+        }
+    }
+}
