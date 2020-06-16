fa583a989f5fab583e29382065adb68d6671daa6
Piotr Maziarz <piotrx.maziarz@linux.intel.com>
2020-06-16 09:24:34 +0200

    nhltdecode: Initial commit
    Add project files.
    
    Signed-off-by: Piotr Maziarz <piotrx.maziarz@linux.intel.com>
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

---
+++ nhltdecode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace nhltdecode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
