cb2fee56cfb1c942c4aaaf2ffdb5849f28f06ede
Piotr Maziarz <piotrx.maziarz@linux.intel.com>
2020-11-13 18:21:04 +0100

    nhltdecode: Implement command line interface
    Add parameters and help to have proper CLI for the application.
    
    Signed-off-by: Piotr Maziarz <piotrx.maziarz@linux.intel.com>
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

--- nhltdecode/Program.cs
+++ nhltdecode/Program.cs
@@ -8,22 +8,76 @@ namespace nhltdecode
     {
         static void Main(string[] args)
         {
-            string input = args[0], output = args[1];
+            if (args.Length == 0)
+            {
+                PrintHelp();
+                return;
+            }
 
-            var reader = new BinaryReader(new FileStream(input, FileMode.Open, FileAccess.Read),
+            string input = "", output = "";
+            bool decode = false;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Equals("-h") || args[i].Equals("--help"))
+                {
+                    PrintHelp();
+                    return;
+                }
+                if ((args[i].Equals("-c") || args[i].Equals("--compile")) && (i + 1) < args.Length)
+                {
+                    input = args[i + 1];
+                    i++;
+                }
+                else if ((args[i].Equals("-d") || args[i].Equals("--decode")) && (i + 1) < args.Length)
+                {
+                    input = args[i + 1];
+                    i++;
+                    decode = true;
+                }
+                else if ((args[i].Equals("-o") || args[i].Equals("--output")) && (i + 1) < args.Length)
+                {
+                    output = args[i + 1];
+                    i++;
+                }
+            }
+
+            if (input.Length == 0 || output.Length == 0)
+            {
+                PrintHelp();
+                Console.WriteLine("Please specify input and output files");
+                return;
+            }
+
+            if (decode)
+            {
+                var reader = new BinaryReader(new FileStream(input, FileMode.Open, FileAccess.Read),
                                               System.Text.Encoding.ASCII);
-            var table = new NHLT();
-            table.ReadFromBinary(reader);
-            reader.Close();
+                var table = new NHLT();
+                table.ReadFromBinary(reader);
+                reader.Close();
 
-            var xtable = NhltXml.FromNative(table);
+                var xtable = NhltXml.FromNative(table);
 
-            var xs = new XmlSerializer(typeof(NhltXml));
-            TextWriter writer = new StreamWriter(output);
+                var xs = new XmlSerializer(typeof(NhltXml));
+                TextWriter writer = new StreamWriter(output);
 
-            xs.Serialize(writer, xtable);
+                xs.Serialize(writer, xtable);
 
-            writer.Close();
+                writer.Close();
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        static private void PrintHelp()
+        {
+            Console.WriteLine("nhltdecode");
+            Console.WriteLine("-h, --help              help");
+            Console.WriteLine("-c, --compile=FILE      compile XML file");
+            Console.WriteLine("-d, --decode=FILE       decode NHLT binary file");
+            Console.WriteLine("-o, --output=FILE       set output file");
         }
     }
 }
