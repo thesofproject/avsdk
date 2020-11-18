bf318f7891796b64fd6a79f20e5d11090aec07b8
Cezary Rojewski <cezary.rojewski@intel.com>
2020-11-18 10:32:22 +0100

    fwlog: Implement Linux specialized fileupdate_watcher
    Add fileupdate_watcher_linux class which makes use of native API to
    monitor content update for subscribed file.
    
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

---
+++ include/fileupdate_listener_linux.hpp
@@ -0,0 +1,27 @@
+#if defined(__linux__)
+
+#ifndef AVS_FILEUPDATE_LISTENER_LINUX_HPP
+#define AVS_FILEUPDATE_LISTENER_LINUX_HPP
+
+#include <string>
+#include "ifileupdate_listener.hpp"
+
+class fileupdate_listener_linux : public ifileupdate_listener {
+public:
+	fileupdate_listener_linux();
+	virtual ~fileupdate_listener_linux();
+
+	virtual bool subscribe(const std::string &fullpath) override;
+	virtual void unsubscribe() override;
+	virtual int wait_for_signal() override;
+
+private:
+	int fd;
+	int wd;
+	fd_set readfds;
+	std::string filename;
+};
+
+#endif // AVS_FILEUPDATE_LISTENER_LINUX_HPP
+
+#endif // __linux__
---
+++ src/fileupdate_listener_linux.cpp
@@ -0,0 +1,93 @@
+#if defined(__linux__)
+
+#include <boost/filesystem/path.hpp>
+#include <iostream>
+#include <string>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include "fileupdate_listener_linux.hpp"
+
+// buffer should be large enough to hold a range of
+// inotify_event instances returned to read()
+#define BUF_LEN ((sizeof(struct inotify_event) + FILENAME_MAX) * 1024)
+
+fileupdate_listener_linux::fileupdate_listener_linux()
+{
+	fd = inotify_init();
+	if (fd < 0)
+		std::cerr << "inotify_init failed: " << errno << std::endl;
+
+	FD_ZERO(&readfds);
+	wd = EINVAL;
+}
+
+fileupdate_listener_linux::~fileupdate_listener_linux()
+{
+	unsubscribe();
+
+	int ret = close(fd);
+	if (ret)
+		std::cerr << "close failed: " << errno << std::endl;
+}
+
+bool fileupdate_listener_linux::subscribe(const std::string &fullpath)
+{
+	if (wd >= 0)
+		return false; // nothing to do
+
+	boost::filesystem::path p(fullpath);
+	filename = p.filename().string();
+
+	wd = inotify_add_watch(fd, p.parent_path().c_str(), IN_MODIFY);
+	if (wd < 0) {
+		std::cerr << "inotify_add_watch failed: " << errno << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+void fileupdate_listener_linux::unsubscribe()
+{
+	if (wd < 0)
+		return; // nothing to do
+	if (inotify_rm_watch(fd, wd))
+		std::cerr << "inotify_rm_watch failed: " << errno << std::endl;
+	wd = EINVAL;
+}
+
+int fileupdate_listener_linux::wait_for_signal()
+{
+	FD_SET(fd, &readfds);
+
+again:
+	int ret = select(fd + 1, &readfds, NULL, NULL, NULL);
+	if (ret < 0) {
+		std::cerr << "select failed: " << errno << std::endl;
+		return errno;
+	}
+
+	if (!FD_ISSET(fd, &readfds))
+		goto again; // no events to process
+
+	char *buf = new char[BUF_LEN];
+	ssize_t len, n = 0;
+		
+	len = read(fd, buf, BUF_LEN);
+
+	while (n < len) {
+		struct inotify_event *event = (struct inotify_event *)&buf[n];
+
+		// test if we can stop blocking
+		if (!filename.compare(event->name))
+			return 0;
+		n += sizeof(*event) + event->len;
+	}
+
+	goto again;
+}
+
+#endif
