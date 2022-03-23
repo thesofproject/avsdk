25ed88a01f40a064e08095b61d2803ec2b2146f5
Piotr Maziarz <piotrx.maziarz@linux.intel.com>
2022-03-23 23:06:13 +0100

    probe2wav: Add command line interface
    Introduce and consume program arguments to implement CLI.
    
    Signed-off-by: Piotr Maziarz <piotrx.maziarz@linux.intel.com>

--- probe2wav/Program.cs
+++ probe2wav/Program.cs
@@ -6,7 +6,57 @@ namespace ProbeExtractor
     {
         static int Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                PrintHelp();
+                return 0;
+            }
+
+            string input = "";
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i].Equals("-h") || args[i].Equals("--help"))
+                {
+                    PrintHelp();
+                    return 0;
+                }
+                else
+                {
+                    input = args[i];
+                }
+            }
+            WavProbeExtractor extractor = new WavProbeExtractor(input);
+
+            try
+            {
+                extractor.ConvertDataToWav();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ProbeExtractor failed. Message: {ex.Message}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Inner exception: {ex.InnerException.Message}");
+                    Console.WriteLine("Inner stack trace:");
+                    Console.WriteLine(ex.InnerException.StackTrace);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Stack trace:");
+                Console.WriteLine(ex.StackTrace);
+                return 1;
+            }
             return 0;
+        }
+
+        static private void PrintHelp()
+        {
+            Console.WriteLine("ProbeExtractor");
+            Console.WriteLine("Usage: ProbeExtractor [Options][File]");
+            Console.WriteLine("Options:");
+            Console.WriteLine("-h, --help         help");
         }
     }
 }
