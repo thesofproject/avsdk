f336db824c8e12fb7aeca69594bd15ff73fe98cd
Piotr Maziarz <piotrx.maziarz@linux.intel.com>
2022-06-10 10:13:26 +0200

    probe2wav: Add struct for chunk header
    This struct represents chunk header and additional properties of it.
    
    Signed-off-by: Piotr Maziarz <piotrx.maziarz@linux.intel.com>

---
+++ probe2wav/ChunkHeader.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ProbeExtractor
+{
+    internal static class Constants
+    {
+        internal static readonly uint SyncPattern = 0xBABEBEBA;
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct ChunkHeader
+    {
+        public uint ProbeId;
+        public uint ProbeFormat;
+        public ulong Timestamp;
+        public uint DataSize;
+
+        public string ProbeIdStr => "0x" + ProbeId.ToString("X8").TrimStart('0');
+
+        // While checksum theoretically is 8 bytes only least significant 4 bytes contains valid data.
+        // Most significant 4 bytes should be equal to 0.
+        public ulong ExpectedChecksum =>
+                (ProbeId + ProbeFormat + Timestamp + DataSize + Constants.SyncPattern) & 0xFFFFFFFF;
+
+        public static ChunkHeader Create(BinaryReader binaryReader)
+        {
+            ChunkHeader header;
+            header.ProbeId = binaryReader.ReadUInt32();
+            header.ProbeFormat = binaryReader.ReadUInt32();
+            header.Timestamp = binaryReader.ReadUInt64();
+            header.DataSize = binaryReader.ReadUInt32();
+
+            return header;
+        }
+    }
+}
