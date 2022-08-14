dd5a2f4974e91758bf6b87746d8ee26801c768ca
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2022-08-14 20:20:19 +0200

    ucm: Communicate license information with SPDX
    Reduce license boilerplate by using SPDX identifiers.
    
    Signed-off-by: Cezary Rojewski <cezary.j.rojewski@gmail.com>

--- NUcmSerializer/SectionComparer.cs
+++ NUcmSerializer/SectionComparer.cs
@@ -3,12 +3,7 @@
 //
 // Author: Cezary Rojewski <cezary.rojewski@intel.com>
 //
-// This program is free software; you can redistribute it and/or modify it
-// under the terms and conditions of the MIT License.
-//
-// This program is distributed in the hope it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.
+// SPDX-License-Identifier: MIT
 //
 
 using System.Collections.Generic;
